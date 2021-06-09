c83b87888b502d8c7e4c3173ae69bbca8df085f7
atlas25git <atlas.sarthak@gmail.com>
2021-06-09 22:30:54 +0530

    nQueen

--- Day1/16ChessBoard.cpp
+++ Day1/16ChessBoard.cpp
@@ -8,6 +8,7 @@ using namespace std;
 #define pb push_back
 const int mod = 1e9 + 7;
 const int N = 100005, M=22;
+
 void solve(){
     int i,j,k,n,m,ans=0,cnt=0,sum=0;
         string s[8];
@@ -19,6 +20,22 @@ void solve(){
             for(i=0;i<8;i++)
             //we've stored input in a strings array
                 ok&=s[i][p[i]]=='.';
+           
+           //checking the given permutation for right diagonal
+                //   /     
+                //  /   diagonal-right
+                // /
+           //as here the sum of indexOnDiag:
+           //                       0,7
+           //                   1,6
+           //                2,5
+           //             3,4      
+           //          4,3
+           //       5,2
+           //    6,1
+           // 7,0
+           //will remain constant hence for any queen's position
+           //once marked would mark the diagonal unusable
             memset(b,0,15);
             for(i=0;i<8;i++)
             {
@@ -26,6 +43,18 @@ void solve(){
                     ok=0;
                 b[i+p[i]]=1;
             }
+
+
+            //checking for the left diagonal
+            // 7,0
+            //    6,1
+            //       5,2
+            //          4,3
+            //             3,4      
+            //                2,5
+            //                   1,6
+            //                       0,7
+            //general transformation: {n-i,j}
             memset(b,0,15);
             for(i=0;i<8;i++)
             {
@@ -33,6 +62,8 @@ void solve(){
                     ok=0;
                 b[i+7-p[i]]=1;
             }
+            
+            //incrementing the ans for the feasible permutation.
             ans+=ok;
         }while(next_permutation(p,p+8));
         cout<<ans;        
